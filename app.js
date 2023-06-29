4a52d1ccc08d5b9d48c29d18b20a078f4709eca2
Olga Bazhenova <ocosmiccobalt@gmail.com>
2023-06-30 00:41:28 +0300

    Parse form data & redirect requests

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const fs = require('fs');
 
 const express = require('express');
 
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   const htmlFilePath = path.join(__dirname, 'views', 'index.html');
@@ -22,6 +24,20 @@ app.get('/recommend', (req, res) => {
   const htmlFilePath = path.join(__dirname, 'views', 'recommend.html');
 
   res.sendFile(htmlFilePath);
+});
+
+app.post('/recommend', (req, res) => {
+  const restaurant = req.body;
+  const filePath = path.join(__dirname, 'data', 'restaurants.json');
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  storedRestaurants.push(restaurant);
+
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+  res.redirect('/confirm');
 });
 
 app.get('/confirm', (req, res) => {

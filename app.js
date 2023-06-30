e46c604d98b6ce1a16b3f359303de457b92cc2c8
Olga Bazhenova <ocosmiccobalt@gmail.com>
2023-06-30 03:12:25 +0300

    Render dynamic content with templates

--- app.js
+++ app.js
@@ -16,7 +16,12 @@ app.get('/', (req, res) => {
 });
 
 app.get('/restaurants', (req, res) => {
-  res.render('restaurants');
+  const filePath = path.join(__dirname, 'data', 'restaurants.json');
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  res.render('restaurants', { numberOfRestaurants: storedRestaurants.length });
 });
 
 app.get('/recommend', (req, res) => {

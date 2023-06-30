4c7bcfa7dc79fea4dcc052292d40bd2d17dcac4d
Olga Bazhenova <ocosmiccobalt@gmail.com>
2023-06-30 03:47:14 +0300

    Output repeated content with EJS & loops

--- app.js
+++ app.js
@@ -21,7 +21,10 @@ app.get('/restaurants', (req, res) => {
   const fileData = fs.readFileSync(filePath);
   const storedRestaurants = JSON.parse(fileData);
 
-  res.render('restaurants', { numberOfRestaurants: storedRestaurants.length });
+  res.render('restaurants', {
+    numberOfRestaurants: storedRestaurants.length,
+    restaurants: storedRestaurants
+  });
 });
 
 app.get('/recommend', (req, res) => {
